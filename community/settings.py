94214fea9f08f0c35b5092653bd20bafc5f91c47
Keshav Gupta <66657408+Keshavv-Guptaa@users.noreply.github.com>
2020-12-20 13:44:48 +0530

    Update settings.py

--- community/settings.py
+++ community/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static_files/'
 
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 MEDIA_URL ='/media/'
