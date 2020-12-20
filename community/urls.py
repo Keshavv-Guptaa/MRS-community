bf01284beeebc9ce5dba08806b91460ad546089b
Keshav Gupta <66657408+Keshavv-Guptaa@users.noreply.github.com>
2020-12-20 13:39:19 +0530

    Update urls.py

--- community/urls.py
+++ community/urls.py
@@ -50,7 +50,7 @@ urlpatterns = [
          ),
          name='password_reset_complete'),
     path('', include('blog.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
