0ee9333e6e5f2eedd6b21a86c9f8f96428203193
Keshav Gupta <66657408+Keshavv-Guptaa@users.noreply.github.com>
2021-01-24 17:28:34 +0530

    Update settings.py

--- community/settings.py
+++ community/settings.py
@@ -138,8 +138,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "adkmrs2020@gmail.com"
-EMAIL_HOST_PASSWORD = "CHqY8pLav4SDWxv"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_SSL = False
 
 django_heroku.settings(locals())
